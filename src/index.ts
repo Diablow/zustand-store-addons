eeaaf1664edced3bd89077c7bf0a49735de271ff
Luis Silva <lurosilo@gmail.com>
2020-10-01 08:18:03 -0500

    computed properties' getter with set/get

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-import create, { State, StateSelector, EqualityChecker, PartialState, GetState, Subscribe, Destroy, StoreApi } from 'zustand';
+import create, {
+  State,
+  StateSelector,
+  EqualityChecker,
+  PartialState,
+  GetState,
+  Subscribe,
+  Destroy,
+  StoreApi,
+} from 'zustand';
 import shallow from 'zustand/shallow';
 import flow from 'lodash/flow';
 // @ts-ignore
@@ -6,35 +15,44 @@ import matchAll from 'string.prototype.matchall';
 
 matchAll.shim();
 
-type TStateRecords = Record<string | number | symbol | any, any>
+type TStateRecords = Record<string | number | symbol | any, any>;
 
 export enum LogLevel {
   None = 'none',
   Diff = 'diff',
-  All = 'all'
+  All = 'all',
 }
 
 interface IAddonsSettings {
-  name?: string,
-  logLevel?: LogLevel | string
+  name?: string;
+  logLevel?: LogLevel | string;
 }
 
 interface SetStateSettings {
-  excludeFromLogs?: boolean | undefined,
-  replace?: boolean | undefined
+  excludeFromLogs?: boolean | undefined;
+  replace?: boolean | undefined;
 }
 
-type SetState<T extends State> = (partial: PartialState<T>, replace?: boolean) => void;
-type SetStateAddons<T extends State> = (partial: PartialState<T>, setSettings?: SetStateSettings) => void;
-declare type StateCreator<T extends State, CustomSetState = SetState<T> | SetStateAddons<T>> = (set: CustomSetState, get: GetState<T>, api: StoreApi<T>) => T;
+type SetState<T extends State> = (
+  partial: PartialState<T>,
+  replace?: boolean
+) => void;
+type SetStateAddons<T extends State> = (
+  partial: PartialState<T>,
+  setSettings?: SetStateSettings
+) => void;
+declare type StateCreator<
+  T extends State,
+  CustomSetState = SetState<T> | SetStateAddons<T>
+> = (set: CustomSetState, get: GetState<T>, api: StoreApi<T>) => T;
 
 type SetStateExtraParam = boolean | undefined | SetStateSettings;
 
 interface IAddons {
-  computed?: TStateRecords,
-  watchers?: TStateRecords,
-  middleware?: Array<any>,
-  settings?: IAddonsSettings
+  computed?: TStateRecords;
+  watchers?: TStateRecords;
+  middleware?: Array<any>;
+  settings?: IAddonsSettings;
 }
 
 function getDeps(fn: (state: State) => any) {
@@ -42,9 +60,7 @@ function getDeps(fn: (state: State) => any) {
   const reg2 = new RegExp(/(?:this\[')(\w+)(?:'\])/g);
   const matches = Array.from(fn.toString().matchAll(reg));
   const matches2 = Array.from(fn.toString().matchAll(reg2));
-  return Array.from(new Set([...matches, ...matches2])).map(
-    (match) => match[1]
-  );
+  return Array.from(new Set([...matches, ...matches2])).map(match => match[1]);
 }
 
 function intersection(aArray: any[], bArray: any) {
@@ -58,20 +74,21 @@ function intersection(aArray: any[], bArray: any) {
 }
 
 export interface UseStore<T extends State> {
-  (): T
-  <U>(selector: StateSelector<T, U> | string, equalityFn?: EqualityChecker<U>): U
-  setState: SetState<T> | SetStateAddons<T>
-  getState: GetState<T>
-  subscribe: Subscribe<T>
-  destroy: Destroy
+  (): T;
+  <U>(
+    selector: StateSelector<T, U> | string,
+    equalityFn?: EqualityChecker<U>
+  ): U;
+  setState: SetState<T> | SetStateAddons<T>;
+  getState: GetState<T>;
+  subscribe: Subscribe<T>;
+  destroy: Destroy;
 }
 
-
-export default function createStore <TState extends TStateRecords>(
+export default function createStore<TState extends TStateRecords>(
   stateInitializer: StateCreator<TState>,
   addons?: IAddons
 ): UseStore<TState> {
-
   let _api: StoreApi<TState>;
   let _originalSetState: SetState<TState>;
   let _computed: TStateRecords = {};
@@ -79,27 +96,32 @@ export default function createStore <TState extends TStateRecords>(
   let _watchers: TStateRecords = {};
   let _settings: IAddonsSettings = {
     name: addons?.settings?.name ?? 'MyStore',
-    logLevel: addons?.settings?.logLevel ?? LogLevel.None
-  }
-
+    logLevel: addons?.settings?.logLevel ?? LogLevel.None,
+  };
 
   function attachMiddleWare(config: StateCreator<TState>) {
-    return (set: SetState<TState>, get: GetState<TState>, api: StoreApi<TState>) => {
+    return (
+      set: SetState<TState>,
+      get: GetState<TState>,
+      api: StoreApi<TState>
+    ) => {
       // Overwrites set method
-      return config((args: any, setSettings: SetStateExtraParam) => {
-        return setMiddleware([args, setSettings], set)
-      }
-      , get, api)
+      return config(
+        (args: any, setSettings: SetStateExtraParam) => {
+          return setMiddleware([args, setSettings], set);
+        },
+        get,
+        api
+      );
     };
-  };
+  }
 
   // Overwrites setState method
   function setState(args: any, setSettings: SetStateExtraParam) {
     setMiddleware([args, setSettings], _originalSetState);
-  };
+  }
 
   function setMiddleware(args: any, set: any) {
-
     const [partialState, setSettings] = args;
 
     let replaceState = false;
@@ -109,30 +131,39 @@ export default function createStore <TState extends TStateRecords>(
       if (typeof setSettings === 'boolean') {
         replaceState = setSettings;
       } else if (typeof setSettings === 'object') {
-        replaceState = setSettings.replace || false
-        excludeFromLogs = setSettings.excludeFromLogs || false
+        replaceState = setSettings.replace || false;
+        excludeFromLogs = setSettings.excludeFromLogs || false;
       }
     }
 
-    const logOperations = (!excludeFromLogs ?? true) && <string>_settings.logLevel !== <string>LogLevel.None;
+    const logOperations =
+      (!excludeFromLogs ?? true) &&
+      <string>_settings.logLevel !== <string>LogLevel.None;
 
     const currentState = _api.getState();
-    const changes = typeof partialState === 'function' ? partialState(currentState) : partialState;
+    const changes =
+      typeof partialState === 'function'
+        ? partialState(currentState)
+        : partialState;
 
-    const group = `${_settings.name} state changed`
-    logOperations && console.group(group)
-    logOperations && <string>_settings.logLevel === <string>LogLevel.All && console.log('Previous State', currentState)
+    const group = `${_settings.name} state changed`;
+    logOperations && console.group(group);
+    logOperations &&
+      <string>_settings.logLevel === <string>LogLevel.All &&
+      console.log('Previous State', currentState);
     logOperations && console.log('Applying', changes);
 
-    let updatedComputed:Record<string | number | symbol, any> = {};
+    let updatedComputed: Record<string | number | symbol, any> = {};
 
-    for (const [compPropName, compPropDeps] of Object.entries(_computedPropDependencies)) {
+    for (const [compPropName, compPropDeps] of Object.entries(
+      _computedPropDependencies
+    )) {
       const needsRecompute = intersection(Object.keys(changes), compPropDeps);
       if (needsRecompute.length > 0) {
         updatedComputed[compPropName] = _computed[compPropName].apply({
           ...currentState,
           ...changes,
-          ...updatedComputed
+          ...updatedComputed,
         });
       }
     }
@@ -144,26 +175,28 @@ export default function createStore <TState extends TStateRecords>(
     const newState = { ...changes, ...updatedComputed };
     set(newState, replaceState);
 
-
-
     for (const [propName, fn] of Object.entries(_watchers)) {
       if (Object.keys(newState).includes(propName)) {
-        logOperations && console.log(`Triggering watcher: ${propName}`)
+        logOperations && console.log(`Triggering watcher: ${propName}`);
         fn.apply({ set: _api.setState, get: _api.getState, api: _api }, [
           newState[propName],
-          currentState[propName]
+          currentState[propName],
         ]);
       }
     }
 
-    logOperations && <string>_settings.logLevel === <string>LogLevel.All && console.log('New State', _api.getState())
-    logOperations && console.groupEnd()
-  };
+    logOperations &&
+      <string>_settings.logLevel === <string>LogLevel.All &&
+      console.log('New State', _api.getState());
+    logOperations && console.groupEnd();
+  }
 
   const middlewareFunctions = (
-    middleware: Array<<TState extends TStateRecords>() => StateCreator<TState>> | undefined,
+    middleware:
+      | Array<<TState extends TStateRecords>() => StateCreator<TState>>
+      | undefined,
     store: StateCreator<TState>
-  ) => middleware ? flow(middleware)(store) : store;
+  ) => (middleware ? flow(middleware)(store) : store);
 
   function configComputed(computed: TStateRecords) {
     let computedToAdd = {};
@@ -175,50 +208,54 @@ export default function createStore <TState extends TStateRecords>(
       }
       computedToAdd = {
         ...computedToAdd,
-        [key]: value.apply({ ..._api.getState(), ...computedToAdd })
+        [key]: value.apply(
+          { ..._api.getState(), ...computedToAdd },
+          { set: _api.setState, get: _api.getState }
+        ),
       };
     }
     if (Object.keys(computedToAdd).length > 0) {
       _api.setState(computedToAdd);
     }
-  };
+  }
 
   function configWatchers(watchers: TStateRecords) {
     for (const [propName, fn] of Object.entries(watchers)) {
       _watchers[propName] = fn;
     }
-  };
+  }
 
-  function _stateCreator <TState extends State>(
+  function _stateCreator<TState extends State>(
     set: SetState<TState> | SetStateAddons<TState>,
     get: GetState<TState>,
     api: StoreApi<TState>
   ): TState {
     _api = api;
-    _originalSetState = {...api}.setState;
+    _originalSetState = { ...api }.setState;
     _api.setState = setState;
-    return {...stateInitializer(<SetState<TState> | SetStateAddons<TState>>set, get, api)}
+    return {
+      ...stateInitializer(
+        <SetState<TState> | SetStateAddons<TState>>set,
+        get,
+        api
+      ),
+    };
   }
 
   const hook = create(
-    attachMiddleWare(
-      middlewareFunctions(
-        addons?.middleware,
-        _stateCreator
-      )
-    )
+    attachMiddleWare(middlewareFunctions(addons?.middleware, _stateCreator))
   );
 
   const stateHook: any = (selector: any, equalityFn?: any) => {
-    if (typeof selector === "string") {
-      if (selector.indexOf(",") !== -1) {
-        const props = selector.split(",").map((part) => part.trim());
-        return hook((state) => props.map((prop) => state[prop]), shallow);
+    if (typeof selector === 'string') {
+      if (selector.indexOf(',') !== -1) {
+        const props = selector.split(',').map(part => part.trim());
+        return hook(state => props.map(prop => state[prop]), shallow);
       }
-      return hook((state) => state[selector], equalityFn);
+      return hook(state => state[selector], equalityFn);
     }
     return hook(selector, equalityFn);
-  }
+  };
 
   // @ts-ignore
   Object.assign(stateHook, _api);
--- test/all.test.tsx
+++ test/all.test.tsx
@@ -1,14 +1,14 @@
 import create from '../src';
 import React from 'react';
 // import ReactDOM from 'react-dom';
-import { render } from '@testing-library/react'
+import { render } from '@testing-library/react';
 
 it('creates a store hook and api object', () => {
-  let params
+  let params;
   const result = create((...args) => {
-    params = args
-    return { value: null }
-  })
+    params = args;
+    return { value: null };
+  });
   expect({ params, result }).toMatchInlineSnapshot(`
     Object {
       "params": Array [
@@ -23,15 +23,15 @@ it('creates a store hook and api object', () => {
       ],
       "result": [Function],
     }
-  `)
+  `);
 });
 
 it('creates a store hook and api object passing empty addons', () => {
-  let params
+  let params;
   const result = create((...args) => {
-    params = args
-    return { value: null }
-  }, {})
+    params = args;
+    return { value: null };
+  }, {});
   expect({ params, result }).toMatchInlineSnapshot(`
     Object {
       "params": Array [
@@ -46,31 +46,34 @@ it('creates a store hook and api object passing empty addons', () => {
       ],
       "result": [Function],
     }
-  `)
+  `);
 });
 
 it('creates a store hook and api object passing addons obj', () => {
-  let params
-  const result = create((...args) => {
-    params = args
-    return { count: 0 }
-  }, {
-    computed: {
-      doubleCount() {
-        return this.count * 2;
-      }
+  let params;
+  const result = create(
+    (...args) => {
+      params = args;
+      return { count: 0 };
     },
-    watchers: {
-      count(newValue: number, oldValue: number) {
-        console.log(newValue, oldValue);
-      }
-    },
-    middleware: [],
-    settings: {
-      name: 'TestingStore',
-      logLevel: 'diff'
+    {
+      computed: {
+        doubleCount() {
+          return this.count * 2;
+        },
+      },
+      watchers: {
+        count(newValue: number, oldValue: number) {
+          console.log(newValue, oldValue);
+        },
+      },
+      middleware: [],
+      settings: {
+        name: 'TestingStore',
+        logLevel: 'diff',
+      },
     }
-  })
+  );
   expect({ params, result }).toMatchInlineSnapshot(`
     Object {
       "params": Array [
@@ -85,125 +88,138 @@ it('creates a store hook and api object passing addons obj', () => {
       ],
       "result": [Function],
     }
-  `)
+  `);
 });
 
 it('uses the store with no args', async () => {
-  const useStore = create((set) => ({
-    count: 1,
-    inc: () => set((state) => ({ count: state.count + 1 })),
-  }), {
-    computed: {
-      doubleCount() {
-        return this.count * 2;
-      }
+  const useStore = create(
+    set => ({
+      count: 1,
+      inc: () => set(state => ({ count: state.count + 1 })),
+    }),
+    {
+      computed: {
+        doubleCount() {
+          return this.count * 2;
+        },
+      },
     }
-  })
+  );
 
   function Counter() {
-    const { count, doubleCount, inc } = useStore()
-    React.useEffect(inc, [])
+    const { count, doubleCount, inc } = useStore();
+    React.useEffect(inc, []);
     return (
       <div>
         <p>count: {count}</p>
         <p>doubleCount: {doubleCount}</p>
       </div>
-    )
+    );
   }
 
   const { findByText } = render(<Counter />);
 
   await findByText('count: 2');
   await findByText('doubleCount: 4');
-})
-
+});
 
 it('uses the store with selectors', async () => {
-  const useStore = create((set, get) => ({
-    count: 1,
-    inc: () => set({ count: get().count + 1 }),
-  }), {
-    computed: {
-      doubleCount() {
-        return this.count * 2;
-      }
+  const useStore = create(
+    (set, get) => ({
+      count: 1,
+      inc: () => set({ count: get().count + 1 }),
+    }),
+    {
+      computed: {
+        doubleCount() {
+          return this.count * 2;
+        },
+      },
     }
-  })
+  );
 
   function Counter() {
-    const count = useStore((s) => s.count)
-    const doubleCount = useStore((s) => s.doubleCount)
-    const inc = useStore((s) => s.inc)
-    React.useEffect(inc, [])
+    const count = useStore(s => s.count);
+    const doubleCount = useStore(s => s.doubleCount);
+    const inc = useStore(s => s.inc);
+    React.useEffect(inc, []);
     return (
       <div>
         <p>count: {count}</p>
         <p>doubleCount: {doubleCount}</p>
       </div>
-    )
+    );
   }
 
-  const { findByText } = render(<Counter />)
+  const { findByText } = render(<Counter />);
 
   await findByText('count: 2');
   await findByText('doubleCount: 4');
-})
+});
 
 it('uses the store with simplified fetch', async () => {
-  const useStore = create((set) => ({
-    count: 1,
-    inc: () => set((state) => ({ count: state.count + 1 })),
-  }), {
-    computed: {
-      doubleCount() {
-        return this.count * 2;
-      }
+  const useStore = create(
+    set => ({
+      count: 1,
+      inc: () => set(state => ({ count: state.count + 1 })),
+    }),
+    {
+      computed: {
+        doubleCount() {
+          return this.count * 2;
+        },
+      },
     }
-  })
+  );
 
   function Counter() {
     const [count, doubleCount, inc] = useStore('count, doubleCount, inc');
-    React.useEffect(inc, [])
+    React.useEffect(inc, []);
     return (
       <div>
         <p>count: {count}</p>
         <p>doubleCount: {doubleCount}</p>
       </div>
-    )
+    );
   }
 
-  const { findByText } = render(<Counter />)
+  const { findByText } = render(<Counter />);
 
   await findByText('count: 2');
   await findByText('doubleCount: 4');
-})
+});
 
 it('uses the store with simplified fetch and watchers', async () => {
-  const useStore = create((set) => ({
-    count: 1,
-    inc: () => set((state) => ({ count: state.count + 1 })),
-    moreThan5: false,
-  }), {
-    computed: {
-      doubleCount() {
-        return this.count * 2;
+  const useStore = create(
+    set => ({
+      count: 1,
+      inc: () => set(state => ({ count: state.count + 1 })),
+      moreThan5: false,
+    }),
+    {
+      computed: {
+        doubleCount() {
+          return this.count * 2;
+        },
+        total() {
+          return this.count + this.doubleCount;
+        },
       },
-      total() {
-        return this.count + this.doubleCount;
-      }
-    },
-    watchers: {
-      total(newValue: number, oldValue: number) {
-        if (newValue > 5 && oldValue <= 5) {
-          this.set({ moreThan5: true })
-        }
-      }
+      watchers: {
+        total(newValue: number, oldValue: number) {
+          if (newValue > 5 && oldValue <= 5) {
+            this.set({ moreThan5: true });
+          }
+        },
+      },
     }
-  })
+  );
 
   function Counter() {
-    const [count, doubleCount, total, moreThan5, inc] = useStore('count, doubleCount, total, moreThan5, inc');
-    React.useEffect(inc, [])
+    const [count, doubleCount, total, moreThan5, inc] = useStore(
+      'count, doubleCount, total, moreThan5, inc'
+    );
+    React.useEffect(inc, []);
     return (
       <div>
         <p>count: {count}</p>
@@ -211,47 +227,52 @@ it('uses the store with simplified fetch and watchers', async () => {
         <p>total: {total}</p>
         {moreThan5 && <p>More than 5</p>}
       </div>
-    )
+    );
   }
 
-  const { findByText } = render(<Counter />)
+  const { findByText } = render(<Counter />);
 
   await findByText('count: 2');
   await findByText('doubleCount: 4');
   await findByText('total: 6');
   await findByText('More than 5');
-})
+});
 
 it('uses the store with simplified fetch and watchers but replacing state from watcher', async () => {
-  const useStore = create((set) => ({
-    count: 1,
-    inc: () => set((state) => ({ count: state.count + 1 })),
-    moreThan5: false,
-  }), {
-    computed: {
-      doubleCount() {
-        return this.count * 2;
+  const useStore = create(
+    set => ({
+      count: 1,
+      inc: () => set(state => ({ count: state.count + 1 })),
+      moreThan5: false,
+    }),
+    {
+      computed: {
+        doubleCount() {
+          return this.count * 2;
+        },
+        total() {
+          return this.count + this.doubleCount;
+        },
       },
-      total() {
-        return this.count + this.doubleCount;
-      }
-    },
-    watchers: {
-      total(newValue: number, oldValue: number) {
-        if (newValue > 5 && oldValue <= 5) {
-          this.set({ moreThan5: true }, { replace: true })
-        }
-      }
-    },
-    settings: {
-      name: 'rudeTest',
-      logLevel: 'all'
+      watchers: {
+        total(newValue: number, oldValue: number) {
+          if (newValue > 5 && oldValue <= 5) {
+            this.set({ moreThan5: true }, { replace: true });
+          }
+        },
+      },
+      settings: {
+        name: 'rudeTest',
+        logLevel: 'all',
+      },
     }
-  })
+  );
 
   function Counter() {
-    const [count, doubleCount, total, moreThan5, inc] = useStore('count, doubleCount, total, moreThan5, inc');
-    React.useEffect(inc, [])
+    const [count, doubleCount, total, moreThan5, inc] = useStore(
+      'count, doubleCount, total, moreThan5, inc'
+    );
+    React.useEffect(inc, []);
     return (
       <div>
         <p>count: {count}</p>
@@ -259,14 +280,11 @@ it('uses the store with simplified fetch and watchers but replacing state from w
         <p>total: {total}</p>
         {moreThan5 && <p>More than 5</p>}
       </div>
-    )
+    );
   }
 
-  const { findByText } = render(<Counter />)
+  const { findByText } = render(<Counter />);
 
   await findByText('More than 5');
   expect(useStore.getState()).toEqual({ moreThan5: true });
-})
-
-
-
+});
